46cf66899410cf2c55a139c38f8774152881e3ee
Vitaly <vitalikd13@gmail.com>
2024-11-03 17:11:59 +0400

    fix: update layout, fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,52 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { IconWallet } from "src/components/icons/wallet";
+import { Button } from "src/components/ui/button";
 import { Toaster } from "src/components/ui/sonner";
 
 const gilroy = localFont({
-  src: "./fonts/Gilroy.ttf",
+  src: [
+    {
+      path: "./fonts/Gilroy.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Gilroy-Italic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Gilroy-SemiBold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Gilroy-Bold.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
   variable: "--font-gilroy",
-  weight: "100 900",
 });
+
 const gtWalsheimPro = localFont({
-  src: "./fonts/GTWalsheimPro.woff",
+  src: [
+    {
+      path: "./fonts/GTWalsheimPro.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/GTWalsheimPro-Bold.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
   variable: "--font-gtWalsheimPro",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -28,9 +63,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${gilroy.variable} ${gtWalsheimPro.variable} antialiased font-body`}
+        className={`${gilroy.variable} ${gtWalsheimPro.variable} font-body antialiased`}
       >
-        {children}
+        <header className="sticky top-0 z-50 w-full border-b border-border bg-background">
+          <div className="container mx-auto flex h-24 max-w-screen-2xl items-center px-12">
+            <div>
+              <NextLink
+                className="mr-4 flex items-center space-x-2 lg:mr-6"
+                href="/"
+              >
+                <NextImage
+                  src="/logo.png"
+                  alt="logo"
+                  width={0}
+                  height={0}
+                  className="h-6 w-6"
+                />
+              </NextLink>
+            </div>
+            <nav className="ml-auto flex items-center">
+              <Button variant="default">
+                <IconWallet className="h-6 w-6" />
+                <span className="sr-only">Wallet Connect</span>
+              </Button>
+            </nav>
+          </div>
+        </header>
+        <main>
+          {/*  */}
+          {children}
+        </main>
         <Toaster />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { IconSolana } from "src/components/icons/networks/solana";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <div className="flex items-center space-x-4 rounded-md border p-4">
           <IconSolana />
           <div className="flex-1 space-y-1">
